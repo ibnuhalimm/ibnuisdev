33ac9534d326847f03b7b8374b14dcb8ae363fec
Ibnu Halim Mustofa <ibnuhalimm@gmail.com>
2021-02-23 11:23:37 +0700

    fix label field component

--- resources/views/components/form-label-inline.blade.php
+++ resources/views/components/form-label-inline.blade.php
@@ -1,4 +1,4 @@
-<div class="flex flex-col md:flex-row items-center mb-6">
+<div class="flex flex-col md:flex-row items-start mb-6">
     <label class="block w-full @if ($size == 'large') xl:w-1/4 @else xl:w-1/3 @endif text-gray-700 md:text-right md:px-3 mb-2 md:mb-0 md:pt-2">
         {!! $text !!}
 
