868ae04a2fe9fbf9ed52e8f31288bc5bf134211d
Ibnu Halim Mustofa <ibnuhalimm@gmail.com>
2021-03-07 16:27:30 +0700

    make padding-bottom of portfolio higher

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -139,7 +139,7 @@
         </x-frontend-container>
     </section>
 
-    <section class="py-10 bg-ib-four">
+    <section class="pt-10 pb-28 bg-ib-four">
         <x-frontend-container>
             <h2 class="mb-3">
                 <span class="font-bold text-4xl block mb-1">
