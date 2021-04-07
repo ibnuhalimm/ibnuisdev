1227952f1150bfa7e13b94d2031459c1e2879eb1
Ibnu Halim Mustofa <ibnuhalimm@gmail.com>
2021-04-07 08:46:52 +0700

    rewriting skill factory

--- database/factories/SkillFactory.php
+++ database/factories/SkillFactory.php
@@ -1,13 +1,30 @@
 <?php
 
-/** @var \Illuminate\Database\Eloquent\Factory $factory */
+namespace Database\Factories;
 
-use App\Skill;
-use Faker\Generator as Faker;
+use App\Models\Skill;
+use Illuminate\Database\Eloquent\Factories\Factory;
 
-$factory->define(Skill::class, function (Faker $faker) {
-    return [
-        'name' => $faker->randomElement(['HTML', 'Javascript', 'CSS', 'Git']),
-        'order_number' => $faker->numberBetween(1, 4)
-    ];
-});
+class SkillFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Skill::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'flag_type' => $this->faker->randomElement([ Skill::FLAG_TYPE_DAY_TO_DAY, Skill::FLAG_TYPE_EXPERIENCE ]),
+            'name' => $this->faker->randomElement(['HTML', 'Javascript', 'CSS', 'Git', 'Laravel', 'Codeigniter', 'ReactJs', 'SCSS', 'TailwindCss', 'Bootstrap']),
+            'order_number' => $this->faker->numberBetween(1, 10)
+        ];
+    }
+}
