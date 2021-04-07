f47e8df186d0c3777653f1a538987a1666915cf6
Ibnu Halim Mustofa <ibnuhalimm@gmail.com>
2021-04-07 08:08:18 +0700

    remove section menu on admin panel

--- resources/views/dashboard/__include/sidebar_homepage_content.blade.php
+++ resources/views/dashboard/__include/sidebar_homepage_content.blade.php
@@ -1,14 +1,6 @@
 <div class="hidden xl:block xl:w-1/6">
     <ul>
         <li class="block mb-3">
-            <a href="{{ route('dashboard.section.index') }}" class="inline-flex @if (Request::is('home/section*')) text-ib-three @else text-gray-900 @endif hover:text-ib-three">
-                <svg class="w-4 h-4 mr-3" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 5a1 1 0 011-1h14a1 1 0 011 1v2a1 1 0 01-1 1H5a1 1 0 01-1-1V5zM4 13a1 1 0 011-1h6a1 1 0 011 1v6a1 1 0 01-1 1H5a1 1 0 01-1-1v-6zM16 13a1 1 0 011-1h2a1 1 0 011 1v6a1 1 0 01-1 1h-2a1 1 0 01-1-1v-6z" />
-                </svg>
-                Section
-            </a>
-        </li>
-        <li class="block mb-3">
             <a href="{{ route('dashboard.portfolio.index') }}" class="inline-flex @if (Request::is('home/portfolio*')) text-ib-three @else text-gray-900 @endif hover:text-ib-three">
                 <svg class="w-4 h-4 mr-3" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 14v6m-3-3h6M6 10h2a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v2a2 2 0 002 2zm10 0h2a2 2 0 002-2V6a2 2 0 00-2-2h-2a2 2 0 00-2 2v2a2 2 0 002 2zM6 20h2a2 2 0 002-2v-2a2 2 0 00-2-2H6a2 2 0 00-2 2v2a2 2 0 002 2z" />
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -46,16 +46,11 @@
                                     <a href="{{ route('home') }}" class="block xl:px-6 text-white hover:text-gray-300 @if (Request::is('home')) font-bold @endif">Dashboard</a>
                                 </li>
                                 <li class="mb-3 xl:mb-0 xl:inline-block menu-item">
-                                    <a href="{{ route('dashboard.section.index') }}" class="block xl:px-6 text-white hover:text-gray-300 menu-item--link @if (Request::is('home/section') OR Request::is('home/portfolio*') OR Request::is('home/skills*')) font-bold @endif">
+                                    <a href="{{ route('dashboard.portfolio.index') }}" class="block xl:px-6 text-white hover:text-gray-300 menu-item--link @if (Request::is('home/section') OR Request::is('home/portfolio*') OR Request::is('home/skills*')) font-bold @endif">
                                         Homepage Content
                                     </a>
 
                                     <ul class="xl:absolute px-0 bg-ib-one py-2 xl:pt-8 xl:pb-4 md:rounded-bl-md md:rounded-br-md md:shadow-md xl:text-left xl:hidden">
-                                        <li>
-                                            <a href="{{ route('dashboard.section.index') }}" class="block px-4 py-1 text-gray-400 hover:text-gray-100 focus:text-gray-100 xl:text-gray-500 text-sm @if (Request::is('home/section*')) font-bold @endif">
-                                                Section
-                                            </a>
-                                        </li>
                                         <li>
                                             <a href="{{ route('dashboard.portfolio.index') }}" class="block px-4 py-1 text-gray-400 hover:text-gray-100 focus:text-gray-100 xl:text-gray-500 text-sm @if (Request::is('home/portfolio*')) font-bold @endif">
                                                 Portfolio
