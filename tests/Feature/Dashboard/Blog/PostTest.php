48c19570ec15e47d40cea11c576763549e0b6dfc
Ibnu Halim Mustofa <ibnuhalimm@gmail.com>
2021-04-27 09:37:26 +0700

    add unit test for uploading image on tinymce editor

--- tests/Feature/Dashboard/Blog/PostTest.php
+++ tests/Feature/Dashboard/Blog/PostTest.php
@@ -3,6 +3,9 @@
 namespace Tests\Feature\Dashboard\Blog;
 
 use App\Models\User;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 use Tests\TestCase;
 
 class PostTest extends TestCase
@@ -18,11 +21,7 @@ class PostTest extends TestCase
         $this->actingAs($user);
     }
 
-    /**
-     * Can view post table page
-     *
-     * @return void
-     */
+
     public function test_admin_can_view_post_table_page()
     {
         $this->setLoggedInUser();
@@ -32,16 +31,24 @@ class PostTest extends TestCase
             ->assertViewIs('dashboard.blog.post.index');
     }
 
-    /**
-     * Contains dashboard.blog.post.table livewire component
-     *
-     * @return void
-     */
+
     public function test_contains_dashboard_blog_post_table_livewire()
     {
         $this->setLoggedInUser();
 
         $response = $this->get(route('dashboard.post.index'));
         $response->assertSeeLivewire('dashboard.blog.post.table');
+    }
+
+
+    public function test_can_upload_image_on_tinyimce_editor()
+    {
+        $this->setLoggedInUser();
+
+        $image = UploadedFile::fake()->image(Str::random(10) . '.jpg');
+
+        $response = $this->post(route('dashboard.post.upload-image'), [ 'file' => $image ]);
+        $response->assertStatus(200)
+            ->assertJsonStructure(['location']);
     }
 }
