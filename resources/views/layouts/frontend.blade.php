18973f7ef8da8a8eda13457ce78c31726eb29046
Ibnu Halim Mustofa <ibnuhalim@pm.me>
2022-05-13 09:57:32 +0700

    add matomo tracking

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -35,6 +35,22 @@
 
         gtag('config', 'UA-177527651-2');
     </script>
+
+    <!-- Matomo -->
+    <script>
+        var _paq = window._paq = window._paq || [];
+        /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+        _paq.push(['trackPageView']);
+        _paq.push(['enableLinkTracking']);
+        (function() {
+        var u="//analytics.isocoding.id/";
+        _paq.push(['setTrackerUrl', u+'matomo.php']);
+        _paq.push(['setSiteId', '2']);
+        var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+        g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+        })();
+    </script>
+    <!-- End Matomo Code -->
     @endif
     <script>
         const BASE_URL = '{{ url('/') }}';
